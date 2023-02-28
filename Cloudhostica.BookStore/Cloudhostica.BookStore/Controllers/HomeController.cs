f54365b442ea0e87c52d41a1fd047e8e32e5daa1
Sharath Raj <sharathrj143@gmail.com>
2023-02-28 12:08:08 +0530

    Added ViewImports, ViewBag and ViewData

--- Cloudhostica.BookStore/Cloudhostica.BookStore/Controllers/HomeController.cs
+++ Cloudhostica.BookStore/Cloudhostica.BookStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cloudhostica.BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace Cloudhostica.BookStore.Controllers
 {
@@ -6,16 +8,25 @@ namespace Cloudhostica.BookStore.Controllers
     {
         public ViewResult Index()
         {
+            ViewBag.Title = "Book Store - Home Page";
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name="Sharath";
+            ViewBag.Data = data;
+            ViewBag.Type = new BookModel() { Id = 5, Author = "This is Author" };
+            ViewData["property1"] = "Sharath Raj";
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            ViewBag.Title = "Book Store - About Us";
             return View();
         }
 
         public ViewResult ContactUs()
         {
+            ViewBag.Title = "Book Store - Contact Us";
             return View();
         }
     }
